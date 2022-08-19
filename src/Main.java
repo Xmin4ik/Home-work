08b7a3035118dd1f91dec1d93f1d9633b86be3c1
xmin3 <Xmin.34@gmail.com>
2022-08-19 13:12:48 +0300

    Коммит №1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Задача №1");
+        var Box = 3;
+    var box = 7;
+        System.out.println(Box);
+        System.out.println(box);
+    byte a = 3;
+    short b = 22;
+    int c = 70;
+    long d = 151L;
+    float e = 0.2f;
+    double f = -0.11;
+        System.out.println("Целочисленные:" );
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("С плавующей точкой:"  +e +" " +f );
 
     }
 }
